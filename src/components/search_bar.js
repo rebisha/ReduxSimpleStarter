52609d298163af52df185359107e495975a7e1fd
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2018-11-01 17:45:35 +1100

    my files

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,24 +6,19 @@ class SearchBar extends Component {
         // Component has its own pair of constructor, so with super(props) we're just calling the parent methods (parent = component) 
         super(props); 
 
-        // whenever you use state initialize it by creating a new object and assign it to this.state. term is a property name and you can use any other name
+        // whenever you use state initialize it by creating a new object and assign it to 'this.state'. term is a property name and you can use any other name
         this.state = { term: ''}; 
     }
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />
-                Value of the input: {this.state.term}
+                <input 
+                value={this.state.term}
+                onChange={event => this.setState({ term: event.target.value })} />
             </div>
         ); 
     } 
 }
 
-export default SearchBar; // any component that imports Searchbar in our application will get our SearchBar component because of this line.
-
-
-/* this is a function component
-    const SearchBar = () => {  
-        return <input />;
-    }; 
-*/
+// any component that imports Searchbar in our application will get our SearchBar component because of this line.
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_detail';
 
 const API_KEY = 'AIzaSyCCO1ojuipmZnrWThF9kV1_lSCHIZ83Lok';
-// Create a new component. This component should produce some HTML
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );    
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container')); // we're telling react to render a function called App.
+        this.state = { videos: [] }; // videos is a state of type array as it will have a list of videos/data
+        
+        YTSearch({key: API_KEY, term: 'cartoons'}, (videos) => {
+            this.setState({ videos }); // instead of printing the objects in console we are passing the values of search to videos so that it gets updated once the search has 
+                                        // kicked off
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetails video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );  
+    }     
+}
+
+// we're telling react to render a function called App.
+ReactDOM.render(<App />, document.querySelector('.container'));
