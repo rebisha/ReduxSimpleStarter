2100299a9fe589884f9ceab3cddb57fbd70fca2d
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2018-11-13 17:18:14 +1100

    new blog post project

---
+++ src/components/posts_new.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+            <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <Field
+                    label="Title For Post"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
