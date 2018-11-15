927c405f2158f0d828edd0460532caf622aac8a4
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2018-11-15 17:57:06 +1100

    new blog post project

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+
+        // ES6 refactoring which means if the field has been touched or has validation error attach 'has-danger' class, otherwise pass an empty class
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`; 
+
         return (
-            <div className="form-group">
-            <label>{field.label}</label>
+            <div className={className}>
+                 <label>{field.label}</label>
                 <input 
                     className="form-control"
                     {...field.input}
                 />
+
+                {/* if the user has touched it show the error message if not show nothing/ empty string  */}
+                <div className="text-help">
+                    {touched ? error : '' } 
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {       
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title For Post"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -32,11 +53,38 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Save</button>
+                <Link className="btn btn-danger " to="/">
+                    Cancel
+                </Link>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const error = {};
+
+    // validate the inputs from values
+    if(!values.title) {
+        error.title = "Enter a title";
+    }
+
+    if(!values.categories) {
+        error.categories = "Enter a category";
+    }
+
+    if(!values.content) {
+        error.content = "Enter some content";
+    }
+
+    //if errors is empty the form is fine to submit
+    return error;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+    render() {
+        return (
+            <div>
+                Showing the post!!
+            </div>
+        );
+    }
+}
+
+export default PostShow;
