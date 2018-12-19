ab560c5abc5291aaa9597de030889ee824c27b36
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2018-12-19 16:09:59 +1100

    working on react google map with marker

--- src/components/map.js
+++ src/components/map.js
@@ -1,44 +1,73 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 
-const AnyReactComponent = () => <div></div>;
+import model from '../model/marker';
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: -37.8136,
-      lng: 144.9631
-    },
-    zoom: 11
-  };
+class GoogleMapsContainer extends Component {
+  constructor(props) {
+    super(props);
 
-  renderMarkers(map, maps) {
-    let marker = new maps.Marker({
-      position: myLatLng,
-      map,
-      title: 'Hello World!'
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
     });
+  }
+
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   }
 
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAHeLNwr9sFXc4CRam2uE3kvHLVIwlNJV8' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
+        <Map className="map mb-5"
+          google = { this.props.google }
+          onClick = { this.onMapClick }
+          zoom = { 11 }
+          initialCenter = {{ lat: -37.8136, lng: 144.9631 }}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
-      </div>
+          {model.map((item) => (
+            <Marker
+              key = { `marker-${item.id}` }
+              onClick = { this.onMarkerClick }
+              position = {{ lat: item.lat, lng: item.lng }}
+              name = { item.name }  
+              description = { item.description }             
+            />
+          ))}      
+
+          <InfoWindow
+            onOpen={this.windowHasOpened}
+            marker= { this.state.activeMarker }
+            visible= { this.state.showingInfoWindow }
+          >
+          <div className="info-window">
+            <h5 className="text-red">{this.state.selectedPlace.name}</h5>
+            <p>{this.state.selectedPlace.description}</p>
+          </div>
+          </InfoWindow>
+        </Map>
     );
   }
 }
 
-export default SimpleMap;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAHeLNwr9sFXc4CRam2uE3kvHLVIwlNJV8'
+})(GoogleMapsContainer);
---
+++ src/model/marker.js
@@ -0,0 +1,32 @@
+const model = [
+    {
+        id: 1,
+        lat: -37.8136,
+        lng: 144.9631,
+        name: 'Melbourne',
+        description: 'Melbourne CBD'
+    },
+    {
+        id: 2,
+        lat: -37.7810,
+        lng: 144.8320,
+        name: 'Sunshine',
+        description: 'Pace of Sunshine'
+    },
+    {
+        id: 3,
+        lat: -37.7520,
+        lng: 145.0700,
+        name: 'Heidelberg',
+        description: 'Heidelberg Height'
+    },
+    {
+        id: 4,
+        lat: -38.0386,
+        lng: 144.4689,
+        name: 'Avalon',
+        description: 'Avalon Airport'
+    }
+  ];
+  
+  export default model;
